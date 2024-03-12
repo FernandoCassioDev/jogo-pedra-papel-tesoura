34e2ba02665af1705d7decb259a5f75e177dd488
Fernando Cassio <fernandocassiodev@gmail.com>
2024-03-11 22:24:36 -0300

    jogo base criado

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-const choiceOne = document.getElementById("player__choice-one");
-const choiceTwo = document.getElementById("player__choice-two");
-const choiceThree = document.getElementById("player__choice-three");
+var playerName;
+var playerChoice = 0;
+var playerPoints = 0;
 
-const messageInHTML = document.getElementById("message");
-const playerNickName = document.getElementById("player__name");
+var computerChoice = 0;
+var computerPoints = 0;
 
-const computerChoice = choose(1, 3);
-var playerChoice;
-
-function showName(name){
-  playerNickName.innerHTML = name;
-}
+playerName = prompt('digite seu nome');
+message('bem vindo '+ playerName +', está preparado? Escolha uma opção acima...');
+showName(playerName)
 
 //exibe mensagem
 function message(text){
-  messageInHTML.innerHTML = text;
+  document.getElementById("message").innerHTML = text;
+}
+
+function showName(name){
+  document.getElementById("player__name").innerHTML = name;
 }
 
 //sorteia entre dois números
@@ -48,40 +49,72 @@ function calcWinner(player, computer) {
   }
 }
 
+//soma pontos para o player
+function addPointsPlayer(){
+  playerPoints ++
+  document.getElementById('player__points').innerHTML = playerPoints
+}
+
+//soma pontos para o computador
+function addPointsComputer(){
+  computerPoints ++
+  document.getElementById('computer__points').innerHTML = computerPoints
+
+}
+
+//adiciona a classse selected
+function select(tipo, escolha){
+  document.getElementById(tipo + '__choice-' + escolha).classList.add('selected')
+}
+
+function unSelect(tipo, escolha){
+  document.getElementById(tipo + '__choice-' + escolha).classList.remove('selected')
+}
+
 //escolhe a jogada do usuário
 //1 - pedra
 //2 - papel
 //3 - tesoura
 function play(choice) {
   playerChoice = choice;
+  select('player', playerChoice)
   //sortear a jogada do computador
-  computerChoice;
+  computerChoice = choose(1, 3);
+  select('computer', computerChoice)
   //calcular quem ganhou
-  const winner = calcWinner(playerChoice, computerChoice);
+  var winner = calcWinner(playerChoice, computerChoice);
   
   if(winner === 0){
     message('empate')
   }else if(winner === 1){
-    message(`${playerName} venceu`)
+    message('Ponto para ' + playerName)
+    addPointsPlayer()
   }else if(winner === 2){
-    message('computador venceu')
+    message('ponto para o computador')
+    addPointsComputer()
   }
-  //somar pontos
-  //exibir para o usuário(mão)
+
+  setTimeout(function() { 
+    unSelect('player', playerChoice)
+    unSelect('computer', computerChoice)
+
+    message(playerName + 'escolha uma opção')
+
+  }, 
+  3500);
+
+
 }
 
-choiceOne.onclick = function () {
-  play(1);
-};
+document.getElementById("player__choice-1").onclick = function(){
+  play(1)
+}
+document.getElementById("player__choice-2").onclick = function() {
+  play(2)
+}
+document.getElementById("player__choice-3").onclick = function() {
+  play(3)
+}
 
-choiceTwo.onclick = function () {
-  play(2);
-};
 
-choiceThree.onclick = function () {
-  play(3);
-};
 
-const playerName = prompt("digite seu nome");
-message(`bem vindo ${playerName}, está preparado? Escolha uma opção acima...`);
-showName(playerName)
